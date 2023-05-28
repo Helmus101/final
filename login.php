02bd1de89dc1ebe943528e6c981bd33064015fdc
Willem Tjong <willem.n.tjong@gmail.com>
2023-05-28 08:24:17 +0200

    commit

---
+++ login.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Login</title>
+        <link rel="stylesheet" href="style.css">
+        <script src="script.js" defer></script>
+        <meta charset="UTF-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.0/css/line.css" />
+    </head>
+    <body>
+        <nav class="nav">
+            <i class="uil uil-bars navOpenBtn"></i>
+            <a href="#" class="logo">Tjong Partners</a>
+      
+            <ul class="nav-links">
+              <i class="uil uil-times navCloseBtn"></i>
+              <li><a href="index.html">Home</a></li>
+              <li><a href="job.html">Jobs</a></li>
+             
+              <li><a href="login.html">Login</a></li>
+            </ul>
+      
+            <i class="uil uil-search search-icon" id="searchIcon"></i>
+            <div class="search-box">
+              <i class="uil uil-search search-icon"></i>
+              <input type="text" placeholder="Search here..." />
+            </div>
+          </nav>
+          <br>
+          <br><br><br>
+      <div class="form">
+        <div class="container">
+        <h3>Login</h3>
+        <form action="#">
+            <label>Username or Email</label><br>
+            <input type="text" placeholder="Enter your username or email" required><br>
+            <label>Password</label><br>
+            <input type="password" placeholder="Enter your password" required><br>
+            <p>Don't have an account?  <a href="register.html" >Register</a></p><br>
+            <input type="submit">
+        </form>
+        </div></div><br><br>
+        <div class="footer">
+            <div class="column-1">
+                <h3>USEFUL LINKS</h3>
+                <a href="index.html">Home</a>
+                <a href="job.html">Jobs</a>
+                <a href="contact.html">Contact</a>
+                <a href="login.html">Login</a>
+            </div>
+            <div class="column-2">
+                   <h3>CONTACT</h3> 
+                   <p>help@tjongpartners.com</p>
+        
+            </div>
+        </div>
+    </body>
+</html>
